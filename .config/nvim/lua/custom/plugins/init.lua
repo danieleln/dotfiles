07649a252808c421bdadd023e0fed63acdc4b2a5
daniele <daniele.monzani@outlook.com>
2024-04-04 12:13:02 +0200

    feat(nvim): lazy loads chromasync.nvim

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+local REPOS = "~/workspace/projects/repos/"
+
+return {
+	{ dir = REPOS .. "chromasync.nvim" },
+}
