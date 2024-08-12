72462a4f2adca0dcc0565428192926b6e38df439
danieleln <daniele.monzani@outlook.com>
2024-08-12 19:39:30 +0200

    feat(nvim): add tabp/tabn shortcuts
    Add shortcuts to move to the previous/next tab in normal mode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -219,6 +219,10 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Focus previous/next tab
+vim.keymap.set("n", "<C-p>", "<Esc>:tabp<CR>", { desc = "Focus previous tab" })
+vim.keymap.set("n", "<C-n>", "<Esc>:tabn<CR>", { desc = "Focus next tab" })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
